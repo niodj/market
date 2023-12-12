3ca065b29565d1ffa8c7294b53772cfe40baff1b
Anton Potapenko <a0501856767@gmail.com>
2023-12-12 22:59:13 +0100

    add search

--- src/componets/topMenu/Time.tsx
+++ src/componets/topMenu/Time.tsx
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 export const Time = () => {
-  const [time, setTime] = useState(Date.now());
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setTime(Date.now());
+      setTime(new Date());
     }, 1000);
 
-    // Очистка интервала при размонтировании компонента
     return () => clearInterval(intervalId);
-  }, []); // Пустой массив зависимостей означает, что эффект будет запущен только при монтировании компонента
+  }, []);
+
+  const day = time.toLocaleString("en-US", { weekday: "long" });
+  const date = time.toLocaleString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+  const timeString = time.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  });
 
   return (
-    <>
-      <div>{time}</div>
-    </>
+    <div>
+      <div>Day: {day}</div>
+      <div>Date: {date}</div>
+      <div>Time: {timeString}</div>
+    </div>
   );
 };
--- src/componets/topMenu/TopMenu.tsx
+++ src/componets/topMenu/TopMenu.tsx
@@ -1,13 +1,13 @@
-
+import React, { ChangeEvent } from "react";
 import { Time } from "./Time";
-import s from "./TopMenu.module.css"
-export const TopMenu = () => {
+import s from "./TopMenu.module.css";
+import { SerachLine } from "./SerachLine";
 
-    return (
-      <>
-            <div className={s.wrapper}>
-                <Time />
-        </div>
-      </>
-    );
-}
+export const TopMenu = () => {
+  return (
+    <div className={s.wrapper}>
+      <SerachLine />
+      <Time />
+    </div>
+  );
+};
