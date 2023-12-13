c2491bc36e3ea46086407a15e38831cad69db3e3
Anton Potapenko <a0501856767@gmail.com>
2023-12-13 10:36:37 +0100

    fixed products

--- src/componets/product/Products.tsx
+++ src/componets/product/Products.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductType, StoreType } from "../../store";
 import s from "./Products.module.css"
-import { type } from "os";
+
 
 export const Products = () => {
   const dispatch = useDispatch();
@@ -34,76 +34,84 @@ const handleDelete = (id: number) => {
 
   return (
     <div className={s.productsContainer}>
-      <h1>Products</h1>
-      <div className={s.filterSection}>
-        <label>Type:</label>
-        <select
-          value={filterType}
-          onChange={(e) => setFilterType(e.target.value)}
-        >
-          <option value=''>All Types</option>
-          {types.map((type, index) => (
-            <option key={index} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className={s.filterSection}>
-        <label>Specification:</label>
-        <select
-          value={filterSpecification}
-          onChange={(e) => setFilterSpecification(e.target.value)}
-        >
-          <option value=''>All Specifications</option>
-          {specifications.map((specification, index) => (
-            <option key={index} value={specification}>
-              {specification}
-            </option>
-          ))}
-        </select>
+      <div className={s.titleAndfilters}>
+        <span>
+          <h2>Products / {products.length}</h2>
+        </span>
+        <span className={s.filterSection}>
+          <label>Type:</label>
+          <select
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
+          >
+            <option value=''>All Types</option>
+            {types.map((type, index) => (
+              <option key={index} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </span>
+        <div className={s.filterSection}>
+          <label>Specification:</label>
+          <select
+            value={filterSpecification}
+            onChange={(e) => setFilterSpecification(e.target.value)}
+          >
+            <option value=''>All Specifications</option>
+            {specifications.map((specification, index) => (
+              <option key={index} value={specification}>
+                {specification}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className={s.productsTableWrapper}>
         <table className={s.productsTable}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Serial Number</th>
-              <th>Is New</th>
-              <th>Photo</th>
-              <th>Title</th>
-              <th>Type</th>
-              <th>Specification</th>
-              <th>Guarantee Start</th>
-              <th>Guarantee End</th>
-              <th>Price</th>
-              <th>Order</th>
-              <th>Date</th>
-              <th>Action</th>
-            </tr>
-          </thead>
           <tbody>
             {filteredProducts.map((product) => (
               <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.serialNumber}</td>
-                <td>{product.isNew}</td>
-                <td>{product.photo}</td>
+                <td>
+                  {product.status ? (
+                    <div className={s.statusMarkTrue} />
+                  ) : (
+                    <div className={s.statusMarkFalse} />
+                  )}
+                </td>
+                <td>
+                  <img src={product.photo} className={s.photo}></img>
+                </td>
                 <td>{product.title}</td>
+                <td>
+                  {product.status ? (
+                    <div className={s.statusTextTrue}>Free</div>
+                  ) : (
+                    <div className={s.statusTextFalse}>On repear</div>
+                  )}
+                </td>
                 <td>{product.type}</td>
-                <td>{product.specification}</td>
-                <td>{product.guarantee.start}</td>
-                <td>{product.guarantee.end}</td>
+                <td>
+                  {product.isNew === 1 ? <div>New</div> : <div>Used</div>}
+                </td>
+                <td>
+                  <div>from {product.guarantee.start.split(" ")[0]}</div>
+                  <div> to {product.guarantee.end.split(" ")[0]}</div>
+                </td>
+                <td>{product.specification} </td>
+
                 <td>
                   {product.price.map((price, index) => (
-                    <div key={index}>
-                      {price.value} {price.symbol}{" "}
-                      {price.isDefault ? "(Default)" : ""}
+                    <div
+                      key={index}
+                      className={price.isDefault === 1 && s.defaultPrice}
+                    >
+                      {price.value} {price.symbol}
                     </div>
                   ))}
                 </td>
                 <td>{product.order}</td>
-                <td>{product.date}</td>
+                <td>{product.date.split(" ")[0]}</td>
                 <td>
                   <button onClick={() => handleDelete(product.id)}>
                     Delete
